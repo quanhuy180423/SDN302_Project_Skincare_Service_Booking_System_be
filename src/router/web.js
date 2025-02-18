d12d34bf436eccde74b65a9b3ced4f61e45b3894
DinhHoa14 <hoamndse173090@fpt.edu.vn>
2025-02-18 10:17:39 +0700

    Update router & postman

--- src/router/web.js
+++ src/router/web.js
@@ -1,22 +1,19 @@
 import express from 'express';
-import userController from '../controllers/userController';
-import authController from '../controllers/authController';
-import { checkTokenWithCookie, checkAuthentication } from "../Middleware/JWTAction";
+import userRouter from './user';
+import authRouter from './auth';
+import { checkTokenWithCookie, checkAuthentication } from "../middleware/JWTAction";
 require('dotenv').config();
 let router = express.Router();
 let initWebRount = (app) => {
 
-    
-
     // Public routes
-    router.post("/register", authController.register);
-    router.post("/login", authController.login);
-    router.post("/logout", authController.logout);
+    router.use('/auth', authRouter);
+    router.use('/user', userRouter);
 
     // Protected routes
     router.all("*", checkTokenWithCookie, checkAuthentication);
     // router.post("/user", userController.createUser);
-    router.get("/user", userController.getUser);
+    // router.get("/user", userController.getUser);
 
     return app.use("/api/", router);
 }

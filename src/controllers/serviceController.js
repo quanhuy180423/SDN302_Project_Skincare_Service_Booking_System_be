3d166adaf1057ce8483a19e97680f838a72eea92
DinhHoa14 <hoamndse173090@fpt.edu.vn>
2025-02-22 00:34:12 +0700

    Paginate & search service

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -1,104 +1,76 @@
 import { BAD_REQUEST, CREATED, NOT_FOUND, OK } from "../config/response.config";
 import serviceService from "../services/serviceService";
 import catchAsync from "../utils/catchAsync";
+import APIError from "../utils/APIError";
+import { query } from "express";
 
 const serviceController = {
   getAllServices: catchAsync(async (req, res) => {
-    try {
-      const services = await serviceService.getAllServices();
-      return OK(res, "Get all services successfully", services);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
-    }
+    const services = await serviceService.getAllServices(req.query);
+
+    return OK(res, "Get all services successfully", services);
   }),
   getServiceById: catchAsync(async (req, res) => {
-    try {
-      const service = await serviceService.getServiceById(req.params.id);
-      if (!service) {
-        return NOT_FOUND(res, "Service not found");
-      }
-      return OK(res, "Get service successfully", service);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
+    const service = await serviceService.getServiceById(req.params.id);
+
+    if (!service) {
+      throw new APIError(404, "Service not found");
     }
+
+    return OK(res, "Get service successfully", service);
   }),
   createService: catchAsync(async (req, res) => {
-    try {
-      const newService = await serviceService.createService(req.body);
-      return CREATED(res, "Create service successfully", newService);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
+    const newService = await serviceService.createService(req.body);
+
+    if (!newService) {
+      throw new APIError(404, "Create new service failed");
     }
+
+    return CREATED(res, "Create service successfully", newService);
   }),
   updateService: catchAsync(async (req, res) => {
-    try {
-      const updateService = await serviceService.updateService(
-        req.params.id,
-        req.body
-      );
-      return OK(res, "Update service successfully", updateService);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
-    }
+    const serviceID = req.params.id;
+    const updateData = req.body;
+    const result = await serviceService.updateService(serviceID, updateData);
+
+    return OK(res, "Update service successfully", result);
   }),
   deleteService: catchAsync(async (req, res) => {
-    try {
-      const deleteService = await serviceService.deleteService(req.params.id);
-      return OK(res, "Delete service successfully", deleteService);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
-    }
+    const serviceID = req.params.id;
+    const result = await serviceService.deleteService(serviceID);
+
+    return OK(res, "Delete service successfully", result);
   }),
   getAllServicesByAdmin: catchAsync(async (req, res) => {
-    try {
-      const services = await serviceService.getAllServicesByAdmin();
-      return OK(res, "Get all services successfully", services);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
-    }
+    const services = await serviceService.getAllServicesByAdmin(req.query);
+    return OK(res, "Get all services successfully", services);
   }),
   getServiceByIdByAdmin: catchAsync(async (req, res) => {
-    try {
-      const service = await serviceService.getServiceByIdByAdmin(req.params.id);
-      return OK(res, "Get service successfully", service);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
-    }
+    const service = await serviceService.getServiceByIdByAdmin(req.params.id);
+    return OK(res, "Get service successfully", service);
   }),
   updateStatusByAdmin: catchAsync(async (req, res) => {
-    try {
-      const id = req.params.id;
-      const { status } = req.body;
-      const updateStatus = await serviceService.updateStatusByAdmin(id, status);
-      if (!updateStatus) {
-        return NOT_FOUND(res, "Service not found");
-      }
-      return OK(res, "Update status successfully", updateStatus);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
+    const id = req.params.id;
+    const { status } = req.body;
+    const updateStatus = await serviceService.updateStatusByAdmin(id, status);
+    if (!updateStatus) {
+      return NOT_FOUND(res, "Service not found");
     }
+    return OK(res, "Update status successfully", updateStatus);
   }),
   getAllSingleServices: catchAsync(async (req, res) => {
-    try {
-      const services = await serviceService.getAllSingleServices();
-      if (!services) {
-        return NOT_FOUND(res, "Service not found");
-      }
-      return OK(res, "Get all services successfully", services);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
+    const services = await serviceService.getAllSingleServices(query);
+    if (!services) {
+      return NOT_FOUND(res, "Service not found");
     }
+    return OK(res, "Get all services successfully", services);
   }),
   getAllComboServices: catchAsync(async (req, res) => {
-    try {
-      const services = await serviceService.getAllComboServices();
-      if (!services) {
-        return NOT_FOUND(res, "Service not found");
-      }
-      return OK(res, "Get all services successfully", services);
-    } catch (error) {
-      return BAD_REQUEST(res, error.message);
+    const services = await serviceService.getAllComboServices(query);
+    if (!services) {
+      return NOT_FOUND(res, "Service not found");
     }
+    return OK(res, "Get all services successfully", services);
   }),
 };
 
